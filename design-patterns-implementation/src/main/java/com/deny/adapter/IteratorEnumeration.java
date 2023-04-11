9b13835573b0f1a61ce901b7ad3406ca8d89ee57
Damian Marek <zmdddamian@gmail.com>
2023-04-11 18:39:32 +0200

    example of iterator to enumeration adapter

---
+++ design-patterns-implementation/src/main/java/com/deny/adapter/IteratorEnumeration.java
@@ -0,0 +1,22 @@
+package com.deny.adapter;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class IteratorEnumeration implements Enumeration<Object> {
+    private final Iterator<?> iterator;
+
+    public IteratorEnumeration(Iterator<?> iterator) {
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public Object nextElement() {
+        return iterator.next();
+    }
+}
